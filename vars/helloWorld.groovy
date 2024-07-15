384de89137a8980e3734a88a79317b951f448f43
Rick Broker <rbroker@cloudbees.com>
2024-07-15 15:08:28 -0400

    UWM lib updates

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,7 @@
+import com.ibm.icu.text.CharsetDetector -- check csproj/validate json - common step
+@Grab(group='com.ibm.icu', module='icu4j', version='72.1')
+Exception in catch: java.io.NotSerializableException
+
 def call(Map config = [:]) {
     sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
 }
