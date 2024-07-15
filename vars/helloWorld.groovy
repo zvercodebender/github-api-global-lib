30e1dcdb8cad66b6546d84da7db57d03a79f36b3
Rick Broker <rbroker@cloudbees.com>
2024-07-15 15:09:44 -0400

    more changes

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,6 +1,10 @@
-import com.ibm.icu.text.CharsetDetector -- check csproj/validate json - common step
+import com.ibm.icu.text.CharsetDetector 
 @Grab(group='com.ibm.icu', module='icu4j', version='72.1')
 Exception in catch: java.io.NotSerializableException
+
+import groovyx.net.http.RESTClient 
+Exception in catch: java.io.NotSerializableException: groovyx.net.http.RESTClient
+java.io.NotSerializableException: groovyx.net.http.RESTClient
 
 def call(Map config = [:]) {
     sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
