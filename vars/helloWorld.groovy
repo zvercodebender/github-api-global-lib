79ab6802f321f14c27339763444aa1132e7185fc
Rick Broker <rbroker@cloudbees.com>
2024-07-16 15:22:23 -0400

    put some libs back

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -7,6 +7,12 @@ import groovyx.net.http.RESTClient
 //Exception in catch: java.io.NotSerializableException: groovyx.net.http.RESTClient
 //java.io.NotSerializableException: groovyx.net.http.RESTClient
 
+import com.ibm.icu.text.CharsetDetector 
+@Grab(group='com.ibm.icu', module='icu4j', version='72.1')
+
+@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7')
+import groovyx.net.http.RESTClient
+
 def call(Map config = [:]) {
     sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
 }
