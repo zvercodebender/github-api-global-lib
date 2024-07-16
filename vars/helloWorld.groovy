7f9cf2437150ef680787bce069d3544c550dd58f
Rick Broker <rbroker@cloudbees.com>
2024-07-16 15:05:26 -0400

    remove exceptions

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,10 +1,10 @@
 import com.ibm.icu.text.CharsetDetector 
 //import com.ibm.icu.icu4j
 //@Grab(group='com.ibm.icu', module='icu4j', version='72.1')
-Exception in catch: java.io.NotSerializableException
+//Exception in catch: java.io.NotSerializableException
 
 import groovyx.net.http.RESTClient 
-Exception in catch: java.io.NotSerializableException: groovyx.net.http.RESTClient
+//Exception in catch: java.io.NotSerializableException: groovyx.net.http.RESTClient
 java.io.NotSerializableException: groovyx.net.http.RESTClient
 
 def call(Map config = [:]) {
