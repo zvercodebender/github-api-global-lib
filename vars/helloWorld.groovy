32bb236a4b1347e7922174e8720587c8216319d3
Rick Broker <rbroker@cloudbees.com>
2024-07-16 11:11:35 -0400

    remove exception

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -3,8 +3,8 @@ import com.ibm.icu.text.CharsetDetector
 //Exception in catch: java.io.NotSerializableException
 
 import groovyx.net.http.RESTClient 
-Exception in catch: java.io.NotSerializableException: groovyx.net.http.RESTClient
-java.io.NotSerializableException: groovyx.net.http.RESTClient
+// Exception in catch: java.io.NotSerializableException: groovyx.net.http.RESTClient
+// java.io.NotSerializableException: groovyx.net.http.RESTClient
 
 def call(Map config = [:]) {
     sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
