4687f14972630140386e278dfe5a7a5697b902cd
Rick Broker <rbroker@cloudbees.com>
2024-07-17 09:57:12 -0400

    change

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,6 +1,6 @@
-//import com.ibm.icu.text.CharsetDetector 
+import com.ibm.icu.text.CharsetDetector 
 //import com.ibm.icu.icu4j
-//@Grab(group='com.ibm.icu', module='icu4j', version='72.1')
+@Grab(group='com.ibm.icu', module='icu4j', version='72.1')
 //Exception in catch: java.io.NotSerializableException
 
 import groovyx.net.http.RESTClient 
