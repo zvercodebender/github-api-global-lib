de7f42d4584704d25091cd841b569d578666bb5e
Rick Broker <rbroker@cloudbees.com>
2024-07-17 10:58:26 -0400

    remove missing lib

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,12 +1,9 @@
+@Grab(group='com.ibm.icu', module='icu4j', version='72.1')
 import com.ibm.icu.text.CharsetDetector 
-import com.ibm.icu.icu4j
-@Grab(group='com.ibm.icu', module='icu4j', version='75.1')
-//Exception in catch: java.io.NotSerializableException
+//Does not exist: import com.ibm.icu.icu4j
 
 @Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7')
 import groovyx.net.http.RESTClient 
-//Exception in catch: java.io.NotSerializableException: groovyx.net.http.RESTClient
-//java.io.NotSerializableException: groovyx.net.http.RESTClient
 
 def call(Map config = [:]) {
     sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
